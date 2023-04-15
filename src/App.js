b63230d12c3a9113e0dda8e2bcd8b44478da5c3e
Bjay <jayanjay2018@gmail.com>
2023-04-15 09:35:47 +0300

    Added The Nav bar to the page and background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Home from './components/layout/Home';
+import Products from './components/layout/Products';
+import Header from './components/layout/Header';
+import ProductDetails from './components/layout/ProductDetails';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './components/layout/Login';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <main>
+      <Switch>
+            <Route path='/'exact>
+              <Redirect to='/home'/>
+            </Route>
+            <Route path = '/home'>
+              
+                <Home/>
+            </Route>
+            <Route path = '/products' exact>
+              <Products/>
+            </Route>
+            <Route path = '/products/:id'>
+              <ProductDetails/>
+            </Route>
+            <Route path = '/login'>
+              <Login/>
+            </Route>
+      </Switch>
+          
+      </main>
     </div>
   );
 }
---
+++ src/components/layout/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min'
+import './Home.css'
+import Products from "./Products"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='home-container'>
+        <img src='https://m.media-amazon.com/images/I/61Ly9zlsGxL._SX1500_.jpg' 
+        alt='hero image'
+        className='home-image'/>
+        <Products/>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
