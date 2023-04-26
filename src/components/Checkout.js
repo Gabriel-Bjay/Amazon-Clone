306555d33972744bde61e3351e1969d586dea7f5
Bjay <jayanjay2018@gmail.com>
2023-04-26 17:20:08 +0300

    Added checkout component

---
+++ src/components/Checkout.js
@@ -0,0 +1,14 @@
+import "./Checkout.css"
+import React from 'react'
+
+ 
+ const Checkout = () => {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default Checkout
+ 
--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import CurrencyFormat from "react-currency-format"
+import "./Subtotal.css"
 
 const Subtotal = () => {
   return (
     <div className='subtotal'>
       <CurrencyFormat 
-        render={(value) =>
+        render={(value) =>(
             <>
-              <p>Subtotal({})</p>
+              <p>Subtotal({basket.length} items): <strong>{value}</strong></p>
+              <small className='subtotal-gift'><input type='checkbox'/>This Order contains a gift</small>
             </>
-        }
+        )}
+          decimalScale={2}
+          value={getBasketTotal(basket)}
+          displayType={'text'}
+          prefix={'$'}
       />
+
+      <button>Proceed to CheckOut</button>
     </div>
   )
 }
